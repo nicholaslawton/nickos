fac88a890e62452610dcc1583241efac6fc02ee5
Nicholas Lawton <nick.a.lawton@gmail.com>
2022-05-02 12:20:59 +0000

    Missed changed file...

--- iso.nix
+++ iso.nix
@@ -1,8 +1,14 @@
 {config, pkgs, ...}:
 
+let nickos = import ./nickos.nix; in
+
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  environment.systemPackages = [
+    nickos.wifi-connect
   ];
 }
