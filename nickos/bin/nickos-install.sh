d86bb9479bfd6d6c3e640f4acad337bebec0f7a0
Nicholas Lawton <nick.a.lawton@gmail.com>
2022-05-07 13:52:36 +0000

    Remove config path optional argument

--- nickos/bin/nickos-install.sh
+++ nickos/bin/nickos-install.sh
@@ -1,4 +1,4 @@
-usage="Usage: $0 [wifi network name] [wifi password] [config path?]"
+usage="Usage: $0 [wifi network name] [wifi password]"
 
 ssid=${1?$usage}
 psk=${2?$usage}
@@ -8,5 +8,5 @@ sudo mount /dev/disk/by-label/nixos /mnt
 sudo mkdir -p /mnt/boot
 sudo mount /dev/disk/by-label/boot /mnt/boot
 sudo nixos-generate-config --root /mnt
-echo "{ $ssid = { psk = \"$psk\"; }" > $configPath/networking-wireless-networks.nix
-cp /nix/store/*-nickos/configuration.nix $configPath
+echo "{ $ssid = { psk = \"$psk\"; }" > /mnt/etc/nixos/networking-wireless-networks.nix
+cp /nix/store/*-nickos/configuration.nix /mnt/etc/nixos
