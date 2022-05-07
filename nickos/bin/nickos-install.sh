48dfb141004d9751acb3c0ae7bcbd9598ddf2f71
Nicholas Lawton <nick.a.lawton@gmail.com>
2022-05-07 09:49:26 +0000

    Removed optional arguments

--- nickos/bin/nickos-install.sh
+++ nickos/bin/nickos-install.sh
@@ -1,14 +1,12 @@
-usage="Usage: $0 [wifi network name] [wifi password] [config path?] [primary disk label?] [boot disk label?]"
+usage="Usage: $0 [wifi network name] [wifi password] [config path?]"
 
 ssid=${1?$usage}
 psk=${2?$usage}
 configPath=${3-"/mnt/etc/nixos"}
-primary=${4-"nixos"}
-boot=${4-"boot"}
 
-sudo mount /dev/disk/by-label/$primary /mnt
+sudo mount /dev/disk/by-label/nixos /mnt
 sudo mkdir -p /mnt/boot
-sudo mount /dev/disk/by-label/$boot /mnt/boot
+sudo mount /dev/disk/by-label/boot /mnt/boot
 sudo nixos-generate-config --root /mnt
 echo "{ $ssid = { psk = \"$psk\"; }" > $configPath/networking-wireless-networks.nix
 cp /nix/store/*-nickos/configuration.nix $configPath
