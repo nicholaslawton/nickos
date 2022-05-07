fc131803e3a95e27d4e208021d58324d731a8807
Nicholas Lawton <nick.a.lawton@gmail.com>
2022-05-07 09:42:31 +0000

    Mounting and generating config

--- nickos/bin/nickos-install.sh
+++ nickos/bin/nickos-install.sh
@@ -1,12 +1,14 @@
-usage="Usage: $0 [wifi network name] [wifi password] [configPath?]"
+usage="Usage: $0 [wifi network name] [wifi password] [config path?] [primary disk label?] [boot disk label?]"
 
 ssid=${1?$usage}
 psk=${2?$usage}
 configPath=${3-"/mnt/etc/nixos"}
+primary=${4-"nixos"}
+boot=${4-"boot"}
 
-sudo mount /dev/disk/by-label/nixos /mnt
+sudo mount /dev/disk/by-label/$primary /mnt
 sudo mkdir -p /mnt/boot
-sudo mount /dev/disk/by-label/boot /mnt/boot
+sudo mount /dev/disk/by-label/$boot /mnt/boot
 sudo nixos-generate-config --root /mnt
 echo "{ $ssid = { psk = \"$psk\"; }" > $configPath/networking-wireless-networks.nix
-cp /nix/store/*-nickos-configuration.nix $configPath/configuration.nix
+cp /nix/store/*-nickos/configuration.nix $configPath
