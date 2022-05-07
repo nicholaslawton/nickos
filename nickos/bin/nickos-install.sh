10481c2b769003b38cdc89c189ab5078b2c0e429
nicholaslawton <nick.a.lawton@gmail.com>
2022-05-08 00:23:37 +1000

    Remove sudo, to be called with sudo

--- nickos/bin/nickos-install.sh
+++ nickos/bin/nickos-install.sh
@@ -2,11 +2,10 @@ usage="Usage: $0 [wifi network name] [wifi password]"
 
 ssid=${1?$usage}
 psk=${2?$usage}
-configPath=${3-"/mnt/etc/nixos"}
 
-sudo mount /dev/disk/by-label/nixos /mnt
-sudo mkdir -p /mnt/boot
-sudo mount /dev/disk/by-label/boot /mnt/boot
-sudo nixos-generate-config --root /mnt
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+nixos-generate-config --root /mnt
 echo "{ $ssid = { psk = \"$psk\"; }" > /mnt/etc/nixos/networking-wireless-networks.nix
 cp /nix/store/*-nickos/configuration.nix /mnt/etc/nixos
