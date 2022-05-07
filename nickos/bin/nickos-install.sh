7dfb03e9b4c2678457c60e42e623dc05060e60ee
nicholaslawton <nick.a.lawton@gmail.com>
2022-05-08 00:30:58 +1000

    Complete installation

--- nickos/bin/nickos-install.sh
+++ nickos/bin/nickos-install.sh
@@ -9,3 +9,5 @@ mount /dev/disk/by-label/boot /mnt/boot
 nixos-generate-config --root /mnt
 echo "{ $ssid = { psk = \"$psk\"; }" > /mnt/etc/nixos/networking-wireless-networks.nix
 cp /nix/store/*-nickos/configuration.nix /mnt/etc/nixos
+nixos-install
+reboot
