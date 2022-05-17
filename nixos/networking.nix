9573cabec6315a19e700db2139c6f6bc9c534072
nicholaslawton <nick.a.lawton@gmail.com>
2022-05-17 18:34:40 +1000

    DHCP config

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -6,4 +6,10 @@
       psk = "%psk%";
     };
   };
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  useDHCP = false;
+  interfaces.wlp1s0.useDHCP = true;
 }
