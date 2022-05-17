23ebdae41c10039564f2474e67dfd6cf285d77b8
nicholaslawton <nick.a.lawton@gmail.com>
2022-05-17 18:33:50 +1000

    Remove DHCP config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,12 +19,6 @@
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp1s0.useDHCP = true;
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
